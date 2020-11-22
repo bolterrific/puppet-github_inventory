c67989525e9ef3298e5c7f7b5edf8a2e823b7b33
Chris Tessmer <chris.tessmer@onyxpoint.com>
2020-11-22 00:00:39 +0000

    Add capability to set PR checks

--- plans/required_checks.pp
+++ plans/required_checks.pp
@@ -1,16 +1,26 @@
 # Status which checks are required on each repo
 plan github_inventory::required_checks(
-  TargetSpec $targets = get_targets('repo_targets'),
-  Sensitive[String[1]]  $github_api_token = Sensitive.new(system::env('GITHUB_API_TOKEN')),
+  TargetSpec           $targets = get_targets('repo_targets'),
+  Sensitive[String[1]] $github_api_token = Sensitive.new(system::env('GITHUB_API_TOKEN')),
+  Optional[String[1]]  $checks  = undef,
 ){
+
+  if $checks.empty {
+    $method = 'get'
+    $body = undef
+  } else {
+    $method = 'put'
+    $body = $checks.split(',')
+  }
+
   $results = run_task_with(
-    'http_request',
-    $targets,
-    'Get status checks protection for all repos'
+    'http_request', $targets, "${method.capitalize} status checks protection"
   ) |$target| {
     {
      'base_url' => "${target.facts['url']}/",
-     'path'     => 'branches/master/protection/required_status_checks/contexts',
+     'method'   => $method,
+     'body'     => $body,
+     'path'     => "branches/${target.facts['default_branch']}/protection/required_status_checks/contexts",
       'headers' => {
         'Accept'        => 'application/vnd.github.v3+json',
         'Authorization' => "token ${github_api_token.unwrap}",
@@ -18,20 +28,24 @@ plan github_inventory::required_checks(
       'json_endpoint' => true
     }
   }
-  #  #  debug::break()
+
   out::message(format::table({
     title => 'Results',
     head  => ['Repo', 'Required checks'],
     rows  => $results.map |$r| {
-      $body = $r.value['body'] =~ Array ? {
-        true  => $r.value['body'].join(", "),
-        false => String($r.value['body'])[0,60],
+      unless $r.value['status_code'] == 200 {
+        [$r.target.name, format::colorize( "${r.value['body']['message']}", 'red' )]
+      } else {
+        $body = $r.value['body'] =~ Array ? {
+          true  => $r.value['body'].join(", "),
+          false => String($r.value['body'])[0,60],
+        }
+        $checks = $r.value['body'].any |$x| { $x =~ /Travis|travis/ } ? {
+          true    => format::colorize( $body, 'warning' ),
+          default => $body,
+        }
+        [ $r.target.name, $checks ]
       }
-      $checks = $r.value['body'] == ['WIP', 'Travis CI - Pull Request'] ? {
-        false   => format::colorize( $body, 'warning' ),
-        default => $body,
-      }
-      [ $r.target.name, $checks ]
     }
   }))
 
