f4248027e597d487f845d779492b72d9dfc9c0e3
Chris Tessmer <chris.tessmer@onyxpoint.com>
2020-12-29 23:03:33 +0000

    Add target filter logic

--- tasks/resolve_reference.rb
+++ tasks/resolve_reference.rb
@@ -12,9 +12,13 @@ class GithubOrg < TaskHelper
   def task(name: nil, **kwargs)
     Dir["#{kwargs[:extra_gem_path]}/gems/*/lib"].each { |path| $LOAD_PATH << path } # for octokit
 
-    org  = kwargs[:org]
-
+    org              = kwargs[:org]
     github_api_token = kwargs[:github_api_token]
+    archived_repos   = kwargs[:archived_repos]
+    private_repos    = kwargs[:private_repos]
+    transport_type   = kwargs[:transport_type]
+    block_list       = kwargs[:block_list]
+    allow_list       = kwargs[:allow_list]
 
     require 'octokit'
     Octokit.auto_paginate = true
@@ -26,26 +30,34 @@ class GithubOrg < TaskHelper
     )
 
     if @client.user( org )['type'] == 'User'
-      repos = @client.repositories(org)
+      repos = @client.repos(org)
     else
       repos = @client.org_repos(org)
     end
 
-    ## TODO: reject block_listed repos/patterns?
+    repos.reject! do |repo|
+      next(true) if repo.archived && !archived_repos
+      next(true) if repo.private && !private_repos
+      if block_list
+        patterns = block_list.select{|item| item =~ %r[\A/.*/\Z] }
+        next(true) if patterns.any? { |p| repo.name =~ Regexp.new( p.sub(%r[\A/],'').sub(%r[/\Z],'') ) }
+        next(true) if (block_list - patterns).any? { |block_str| repo.name == block_str }
+      end
+      if allow_list
+        patterns = allow_list.select{|item| item =~ %r[\A/.*/\Z] }
+        p_match = patterns.any? { |p| repo.name =~ Regexp.new( p.sub(%r[\A/],'').sub(%r[/\Z],'') ) }
+        s_match = (allow_list - patterns).any? { |allow_str| repo.name == allow_str }
+        next(true) unless (p_match || s_match)
+      end
+    end
 
-    targets = repos.map do |repo|
-      # FIXME: handle interpreters for non-linux OSes
+    targets = repos.sort_by{|repo| repo.name}.map do |repo|
       target = YAML.load <<~YAML
-        name: #{repo['full_name']}
+        name: '#{repo['full_name']}'
         features:
           - puppet-agent
         config:
-          transport: local
-          local:
-            interpreters:
-              '.rb': '/opt/puppetlabs/bolt/bin/ruby'
-            # interpreters: <same as localhost tmpdir>
-            # tmpdir: <same as localhost tmpdir>
+          transport: '#{transport_type}'
         vars: {}
         facts: {}
       YAML
