7436a10a31e463a148aaf0770307d54e215faed6
Chris Tessmer <chris.tessmer@onyxpoint.com>
2020-12-31 23:13:28 +0000

    Fixup example plan

--- plans/count.pp
+++ plans/count.pp
@@ -3,12 +3,8 @@
 # @param targets
 #    By default: `repo_targets` group from inventory
 #
-# @param github_api_token
-#    GitHub API token.  By default, this will use the `GITHUB_API_TOKEN` environment variable.
-#
-plan github_inventory(
+plan github_inventory::count(
   TargetSpec $targets = 'repo_targets',
-  String[1]  $github_api_token = system::env('GITHUB_API_TOKEN'),
 ){
   $repo_targets = get_targets($targets),
   out::message( "Repos: ${targets.size}" )
